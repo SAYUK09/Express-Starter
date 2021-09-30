27445ffbf3059b11a9df2ad3367d6e38cd1f57a4
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-09-30 20:14:27 +0530

    Helmet configured

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const express = require("express");
 const demoRoute = require("./routes/demo.route.js");
 const logger = require("./utils/logger");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
 const { corsOptions } = require("./config/corsConfig");
+
 const { port } = require("./config/envConfig");
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(morgan("tiny"));
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hey There!");
